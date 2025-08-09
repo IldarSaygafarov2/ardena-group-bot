bf08462bff10aa9c49811c9d57eb5810dc2a1998
IldarSaygafarov2 <sayildar17@gmail.com>
2025-08-09 10:40:29 +0500

    added fixes

--- tgbot/handlers/admin/commands.py
+++ tgbot/handlers/admin/commands.py
@@ -1,6 +1,8 @@
 from aiogram import Router, types
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
+
+from database.repo.requests import RequestsRepo
 from tgbot.misc.states import AdminChemistryFilesState
 
 from tgbot.filters.admin import AdminFilter
@@ -17,8 +19,11 @@ async def handle_admin_start_command(message: types.Message):
 
 
 @admin_commands_router.message(Command('generate_report'))
-async def handle_admin_generate_report_command(message: types.Message, state: "FSMContext"):
+async def handle_admin_generate_report_command(message: types.Message, repo: "RequestsRepo", state: "FSMContext"):
     await state.set_state(AdminChemistryFilesState.files)
+    from_db = await repo.chemistry_file.get_all_files()
+    file_path = from_db[0].file_path
+
 
     await message.answer("Отправьте файлы хим составов")
 
